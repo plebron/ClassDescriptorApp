94cdb2dbd179573609d8bfa1c71388af4d1cf7a6
Pedro Lebron <pedrolebron@Pedros-MacBook-Pro.local>
2023-04-07 12:47:13 -0400

    back-end - Implemented fetch for constructor

--- back-end-app/src/main/java/org/classDescriptor/Main.java
+++ back-end-app/src/main/java/org/classDescriptor/Main.java
@@ -5,6 +5,7 @@ classDescriptor;
 import org.classDescriptor.models.Attributes;
 import org.classDescriptor.models.ClassesModel;
 
+import javax.smartcardio.ATR;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,86 +13,88 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Attributes> attributes;
 
-        System.out.println("Hello world!");
-                // Replace this string with the text of the Java class you want to describe
-                String classText = "// Person.txt\n" +
-                        "\n" +
-                        "public class Person {\n" +
-                        "\n" +
-                        " \n" +
-                        "\n" +
-                        "    private int year = 0;\n" +
-                        "\n" +
-                        " \n" +
-                        "\n" +
-                        "    public void getOlder(int year) {\n" +
-                        "\n" +
-                        "        if(year <= 0) {\n" +
-                        "\n" +
-                        "            return;\n" +
-                        "\n" +
-                        "        }\n" +
-                        "\n" +
-                        "        this.year += year;\n" +
-                        "\n" +
-                        "    }\n" +
-                        "\n" +
-                        "    public String eat() {\n" +
-                        "\n" +
-                        "        return eat(\"chicken\");\n" +
-                        "\n" +
-                        "    }\n" +
-                        "\n" +
-                        "    public String eat(String food) {\n" +
-                        "\n" +
-                        "        if(year <= 1) {\n" +
-                        "\n" +
-                        "            return \"eating milk...\";\n" +
-                        "\n" +
-                        "        }\n" +
-                        "\n" +
-                        "        return \"eating \" + food + \"...\";\n" +
-                        "\n" +
-                        "    }\n" +
-                        "\n" +
-                        "}";
+        // Replace this string with the text of the Java class you want to describe
+        String classText = "// Person.txt\n" +
+                "\n" +
+                "public class Person {\n" +
+                "\n" +
+                " \n" +
+                "\n" +
+                "    private int year = 0;\n" +
+                "\n" +
+                " \n" +
+                "\n" +
+                "    public void getOlder(int year) {\n" +
+                "\n" +
+                "        if(year <= 0) {\n" +
+                "\n" +
+                "            return;\n" +
+                "\n" +
+                "        }\n" +
+                "\n" +
+                "        this.year += year;\n" +
+                "\n" +
+                "    }\n" +
+                "\n" +
+                "    public String eat() {\n" +
+                "\n" +
+                "        return eat(\"chicken\");\n" +
+                "\n" +
+                "    }\n" +
+                "\n" +
+                "    public String eat(String food) {\n" +
+                "\n" +
+                "        if(year <= 1) {\n" +
+                "\n" +
+                "            return \"eating milk...\";\n" +
+                "\n" +
+                "        }\n" +
+                "\n" +
+                "        return \"eating \" + food + \"...\";\n" +
+                "\n" +
+                "    }\n" +
+                "\n" +
+                "}";
+
+        String className, scope, constructor;
+        className = scope = constructor = "";
+
+        ArrayList<Attributes> fields = new ArrayList<>();
 
         // Split the class text into lines
         String[] lines = classText.split("\n");
+        for (String line : lines) {
+            className = getClassName(line, className);
+            scope = getScope(line, scope);
+            constructor = getConstructor(line, className, constructor);
+        }
 
         outputFormatter(
-                new ClassesModel(getClassName(lines), getScope(lines), "", getFields(lines)));
+                new ClassesModel(className, scope, constructor, getFields(lines)));
     }
 
-    private static String getClassName(String[] inputLines) {
-        // Identify the name of the class
-        String className = null;
-        for (String line : inputLines) {
-            if (line.contains("class")) {
-                className = line.split(" ")[2];
-                break;
-            }
+    private static String getClassName(String inputLine, String className) {
+        if (inputLine.contains("class")) {
+            return inputLine.split(" ")[2];
         }
         return className;
     }
 
-    private static String getScope(String[] inputLines) {
-        // Determine the class's access level
-        String scope = null;
-        for (String line : inputLines) {
-            if (line.contains("public")) {
-                scope = "public";
-                break;
-            } else if (line.contains("private")) {
-                scope = "private";
-                break;
-            }
+    // Determine the class's access level
+    private static String getScope(String inputLine, String scope) {
+        if (inputLine.contains("public")) {
+            return "public";
+        } else if (inputLine.contains("private")) {
+            return "private";
         }
         return scope;
     }
 
-    private static String getConstructor(String[] inputLines) {
-        return "";
+    private static String getConstructor(String inputLine, String className, String constructor) {
+        if(inputLine.contains(className) && inputLine.contains("(")){
+            return inputLine;
+        }
+        return constructor.isEmpty() ? className : constructor;
     }
 
     private static String getFieldName(String inputLine) {
